86ab3812b2ce822c75d89c4dd5c5401b9a17921f
ioan.cioclut <ioan.cioclut@volantetech.com>
2023-02-01 19:10:34 +0200

    Parameterize topics name

--- src/main/java/com/research/kafkaapp/KafkaTopicConfiguration.java
+++ src/main/java/com/research/kafkaapp/KafkaTopicConfiguration.java
@@ -1,20 +1,33 @@
 package com.research.kafkaapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
+@Slf4j
 public class KafkaTopicConfiguration {
+
+    @Value("${kafka.input.topic}")
+    private String inputTopic;
+
+    @Value("${kafka.output.topic}")
+    private String outputTopic;
 
     @Bean
     public NewTopic inputTopic() {
-        return TopicBuilder.name("transform").partitions(5).replicas(1).build();
+        NewTopic topic = TopicBuilder.name(this.inputTopic).partitions(5).replicas(1).build();
+        log.info("Created topic: {}", topic);
+        return topic;
     }
 
     @Bean
     public NewTopic outputTopic() {
-        return TopicBuilder.name("messageprocessed").partitions(5).replicas(1).build();
+        NewTopic topic = TopicBuilder.name(this.outputTopic).partitions(5).replicas(1).build();
+        log.info("Created topic: {}", topic);
+        return topic;
     }
 }

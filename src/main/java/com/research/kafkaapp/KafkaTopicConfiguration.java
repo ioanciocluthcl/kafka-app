49a629db3ce7b90129f0671c6bb53f97a264aca4
ioan.cioclut <ioan.cioclut@volantetech.com>
2023-04-03 14:42:05 +0300

    Change type

--- src/main/java/com/research/kafkaapp/KafkaTopicConfiguration.java
+++ src/main/java/com/research/kafkaapp/KafkaTopicConfiguration.java
@@ -2,11 +2,8 @@ package com.research.kafkaapp;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 @Slf4j
@@ -19,17 +16,17 @@ public class KafkaTopicConfiguration {
     @Value("${kafka.output.topic}")
     private String outputTopic;
 
-    @Bean
-    public NewTopic inputTopic() {
-        NewTopic topic = TopicBuilder.name(this.inputTopic).partitions(5).replicas(1).build();
-        log.info("Created topic: {}", topic);
-        return topic;
-    }
-
-    @Bean
-    public NewTopic outputTopic() {
-        NewTopic topic = TopicBuilder.name(this.outputTopic).partitions(5).replicas(1).build();
-        log.info("Created topic: {}", topic);
-        return topic;
-    }
+//    @Bean
+//    public NewTopic inputTopic() {
+//        NewTopic topic = TopicBuilder.name(this.inputTopic).partitions(3).replicas(1).build();
+//        log.info("Created topic: {}", topic);
+//        return topic;
+//    }
+//
+//    @Bean
+//    public NewTopic outputTopic() {
+//        NewTopic topic = TopicBuilder.name(this.outputTopic).partitions(3).replicas(1).build();
+//        log.info("Created topic: {}", topic);
+//        return topic;
+//    }
 }
--- src/main/java/com/research/kafkaapp/TestController.java
+++ src/main/java/com/research/kafkaapp/TestController.java
@@ -1,7 +1,6 @@
 package com.research.kafkaapp;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -23,20 +22,23 @@ public class TestController {
 
     private int counter = 1000;
 
+    private long start;
+
     @PostMapping("/messages")
     @Transactional
-    public void sendMessage(@RequestBody MyMessage myMessage) throws JsonProcessingException {
-        ObjectMapper mapper = new ObjectMapper();
-        String value = mapper.writeValueAsString(myMessage);
+    public void sendMessage(@RequestBody String message) throws JsonProcessingException {
         String key = "key" + counter;
         counter++;
-        log.info("Sending on topic {}: {}", topicConfiguration.getInputTopic(), value);
-        kafkaTemplate.send(topicConfiguration.getInputTopic(), key, value);
+        log.info("Sending on topic {}: {}", topicConfiguration.getInputTopic(), message);
+        start = System.currentTimeMillis();
+        kafkaTemplate.send(topicConfiguration.getInputTopic(), key, message);
     }
 
     @Transactional
     @KafkaListener(topics = "#{'${kafka.output.topic}'.split(',')}", groupId = "my-group22")
     public void consume(String message) {
+        long end = System.currentTimeMillis();
+        System.out.println(end - start);
         log.info("Received on {}: {}", topicConfiguration.getOutputTopic(), message);
     }
 
